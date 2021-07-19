9326b941c968462eafd3bfd49c0f20849712003f
mathieu <mathieuvie@gmail.com>
2021-07-19 10:07:24 +0200

    fix: add more tests

--- src/tests/check.spec.js
+++ src/tests/check.spec.js
@@ -31,15 +31,15 @@ const arrayOfDays = [
 
 for (let day = 0; day < 7; day++) {
   switch (day) {
-    // case 0:
-    // case 6:
-    //   test(`Test is Close on ${arrayOfDays[day]}`, () => {
-    //     for (let hour = 0; hour < 24; hour++) {
-    //       const date = dayjs(new Date()).hour(hour).day(day).tz('Europe/Paris')
-    //       expect(isOpen(opening, date)).not.toBe(true)
-    //     }
-    //   })
-    //   break
+    case 0:
+    case 6:
+      test(`Test is Close on ${arrayOfDays[day]}`, () => {
+        for (let hour = 0; hour < 24; hour++) {
+          const date = dayjs(new Date()).tz('Europe/Paris').hour(hour).day(day)
+          expect(isOpen(opening, date)).not.toBe(true)
+        }
+      })
+      break
     case 1:
       test(`Test is ${arrayOfDays[day]}`, () => {
         for (let hour = 0; hour < 24; hour++) {
@@ -55,41 +55,41 @@ for (let day = 0; day < 7; day++) {
       })
       break
 
-    // default:
-    //   test(`Test is ${arrayOfDays[day]}`, () => {
-    //     for (let hour = 0; hour < 24; hour++) {
-    //       const date = dayjs(new Date()).hour(hour).day(day).tz('Europe/Paris')
-    //       if ([10, 11, 14, 15, 16, 17].includes(hour)) {
-    //         expect(isOpen(opening, date)).toBe(true)
-    //       } else {
-    //         // console.log(`${hour}=>${isOpen(opening, date)}`)
-    //         expect(isOpen(opening, date)).not.toBe(true)
-    //       }
-    //     }
-    //   })
+    default:
+      test(`Test is ${arrayOfDays[day]}`, () => {
+        for (let hour = 0; hour < 24; hour++) {
+          const date = dayjs(new Date()).tz('Europe/Paris').hour(hour).day(day)
+          if ([10, 11, 14, 15, 16, 17].includes(hour)) {
+            expect(isOpen(opening, date)).toBe(true)
+          } else {
+            // console.log(`${hour}=>${isOpen(opening, date)}`)
+            expect(isOpen(opening, date)).not.toBe(true)
+          }
+        }
+      })
   }
 }
 
-// test('Test is Mother\'s day 2021 is Open', () => {
-//   opening.holidays.isOpen = false
-//   opening.holidays.checkHolidaysTypes = ['public']
-//   opening.weekDay[7] = '* 16-18 * * *'
-//   // initialize holidayss for US, Lousiana, New Orleans
-//   opening.holidays.country = 'US'
-//   opening.holidays.state = 'la'
-//   opening.holidays.region = 'no'
-//
-//   const date = dayjs('20210509').hour(17).tz('Europe/Paris').format()
-//   expect(isOpen(opening, date)).toBe(true)
-// })
-//
-// test('Test is Mother\'s day 2021 is Close', () => {
-//   opening.holidays.isOpen = false
-//   opening.holidays.checkHolidaysTypes = ['public', 'observance']
-//   opening.weekDay[7] = '* 16-18 * * *'
-//   opening.holidays.country = 'US'
-//   opening.holidays.state = 'la'
-//   opening.holidays.region = 'no'
-//   const date = dayjs('20210509').hour(17).tz('Europe/Paris').format()
-//   expect(isOpen(opening, date)).toBe(false)
-// })
+test('Test is Mother\'s day 2021 is Open', () => {
+  opening.holidays.isOpen = false
+  opening.holidays.checkHolidaysTypes = ['public']
+  opening.weekDay[7] = '* 16-18 * * *'
+  // initialize holidayss for US, Lousiana, New Orleans
+  opening.holidays.country = 'US'
+  opening.holidays.state = 'la'
+  opening.holidays.region = 'no'
+
+  const date = dayjs('20210509').tz('Europe/Paris').hour(17).format()
+  expect(isOpen(opening, date)).toBe(true)
+})
+
+test('Test is Mother\'s day 2021 is Close', () => {
+  opening.holidays.isOpen = false
+  opening.holidays.checkHolidaysTypes = ['public', 'observance']
+  opening.weekDay[7] = '* 16-18 * * *'
+  opening.holidays.country = 'US'
+  opening.holidays.state = 'la'
+  opening.holidays.region = 'no'
+  const date = dayjs('20210509').tz('Europe/Paris').hour(17).format()
+  expect(isOpen(opening, date)).toBe(false)
+})
